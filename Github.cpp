7d9206efe8b1cd0b55cca54f7b46cd846d0ba2ea
nguoitoiyeu <ngocdungvinhquang2@gmail.com>
2025-02-12 20:37:19 +0700

    Add Github.cpp file

---
+++ Github.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define N 1e5
+using namespace std;
+
+typedef vector<int> vector_;
+typedef pair<int, int> pair_;
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << "hello world";
+    return 0;
+}
